fbdfeb56f90b777d40875e1b7dc6e01a48374a08
CBF <147460707+CBF2006@users.noreply.github.com>
2026-04-25 23:01:18 -0400

    Tower Shoots at enemiess

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @export var fire_rate = 1.5
+@export var damage = 10
+
+var projectile_scene = preload("res://scenes/laser.tscn")  # adjust path if needed
 var target = null
 var timer = 0.0
 
@@ -11,7 +14,10 @@ func _process(delta):
 		timer = 0.0
 
 func shoot():
-	print("Shooting enemy!")
+	var p = projectile_scene.instantiate()
+	get_parent().add_child(p)
+	p.global_position = global_position
+	p.target = target
 
 func _on_area_2d_body_entered(body):
 	target = body
