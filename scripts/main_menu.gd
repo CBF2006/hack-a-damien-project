f6054ebeb11949a6b7a6ae2b9e55dd860b35c4a6
CBF <147460707+CBF2006@users.noreply.github.com>
2026-04-26 12:05:56 -0400

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,16 +6,19 @@ func _ready():
 	$TextureRect.position = Vector2.ZERO
 	$VBoxContainer/Button.grab_focus()
 
+	var joy = get_tree().get_first_node_in_group("joystick_controller")
+	if joy:
+		joy.action_pressed.connect(_on_joy_action)
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_settings.tscn")
-
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
-	
+
 func _on_joy_action(action: String) -> void:
 	print("joy action: ", action)
 	if action == "confirm":
